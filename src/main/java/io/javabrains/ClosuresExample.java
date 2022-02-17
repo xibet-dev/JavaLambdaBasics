c3c0fec8d3fa0a09dfbcdf226c1e7af71778b57a
xibet-dev <marco.scibetta.dev@gmail.com>
2022-02-17 14:02:30 +0100

    Closures class added

--- src/main/java/io/javabrains/ClosuresExample.java
+++ src/main/java/io/javabrains/ClosuresExample.java
@@ -1,14 +1,17 @@
 package io.javabrains;
 
+import java.util.Arrays;
+
 public class ClosuresExample {
     public static void main(String[] args) {
         int a = 10;
-        int b = 20;
-        doProcess(a, new Process() {
-            @Override
-            public void process(int i) {
-                System.out.println(i + b);
-            }
+        int b = 20; //--> a "frozen" value (final)
+        //a closure is a function that can use parameters not defined in
+        //the parameter list
+        doProcess(a, i -> {
+            System.out.println(i + b);
+            int c = b + 100;
+            System.out.println("c: " + c);
         });
     }
 
